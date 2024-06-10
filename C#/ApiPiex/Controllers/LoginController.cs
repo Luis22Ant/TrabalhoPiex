6036d47286153fa6a5c8fb08a51ab0992c018f7f
Luis22Ant <luisengenhariaeletronica@gmail.com>
2024-06-10 08:21:41 -0300

    api de login

---
+++ C#/ApiPiex/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiPiex.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+
+        [HttpPost]
+        public IActionResult Login(string login,string senha)
+        {
+
+
+            return Ok(login);
+        }
+    }
+}
--- C#/ApiPiex/obj/Release/net8.0/ApiPiex.AssemblyInfo.cs
+++ C#/ApiPiex/obj/Release/net8.0/ApiPiex.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ApiPiex")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6216b2183aedb641ea44cba243d116259cf05065")]
 [assembly: System.Reflection.AssemblyProductAttribute("ApiPiex")]
