674932aa558b2f57a40faec3c079f9f3cb6dae84
Luis22Ant <luisengenhariaeletronica@gmail.com>
2024-07-03 18:02:46 -0300

    Ajustes

--- C#/ApiProjetoIntegrador/ApiProjetoIntegrador/Communication/Request/Auth/AuthRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ApiProjetoIntegrador.Communication.Request.Auth
-{
-    public class AuthRequest
-    {
-        public string Login { get; set; } = string.Empty;
-        public string Senha { get; set; } = string.Empty;
-    }
-}
